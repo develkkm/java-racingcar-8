5572967440f96e214bbf7ff69108e3f1f455b3c9
develkkm <kkm8905508@daum.net>
2025-10-27 19:32:47 +0900

    refactor(AppConfig): 난수 생성 책임 분리 및 상수 정의
    - 기존 람다식을 사용한 MoveStrategy를 RacingMoveStrategy로 교체
    - 난수 생성 범위 및 임계점을 상수로 정의
    - InputView, OutputView를 private 메서드로 분리하여 캡슐화 강화

--- src/main/java/racingcar/config/AppConfig.java
+++ src/main/java/racingcar/config/AppConfig.java
@@ -1,26 +1,30 @@
 package racingcar.config;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.controller.RacingController;
+import racingcar.model.generator.RandomNumberGenerator;
 import racingcar.model.strategy.MoveStrategy;
+import racingcar.model.strategy.RacingMoveStrategy;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class AppConfig {
+    private static final int MIN_NUMBER = 0;
+    private static final int MAX_NUMBER = 9;
+    private static final int THRESHOLD = 4;
 
     public MoveStrategy moveStrategy() {
-        return () -> Randoms.pickNumberInRange(0, 9) >= 4;
-    }
-
-    public InputView inputView() {
-        return new InputView();
-    }
-
-    public OutputView outputView() {
-        return new OutputView();
+        return new RacingMoveStrategy(new RandomNumberGenerator(MIN_NUMBER,MAX_NUMBER),THRESHOLD);
     }
 
     public RacingController racingController() {
         return new RacingController(inputView(), outputView(), moveStrategy());
+    }
+
+    private InputView inputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
     }
 }
