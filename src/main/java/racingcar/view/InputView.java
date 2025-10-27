f2c3db89ae542ba35554c85c4ef91418ea1dd6bb
develkkm <kkm8905508@daum.net>
2025-10-27 19:39:08 +0900

    style(InputView): 입력 메세지 상수 분리
    - 입력 안내 메시지 문자열을 상수로 추출

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,13 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ROUND_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
     public String readNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
         return Console.readLine();
     }
 
     public String readRound() {
-        System.out.println("시도할 횟수는 몇 회인가요?");
+        System.out.println(INPUT_ROUND_MESSAGE);
         return Console.readLine();
     }
 }
